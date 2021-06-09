ed26a821d475fc76e6c14eecceed51136ca2a64a
Erik Zambrano <Erik.Zambrano@gartner.com>
2021-06-08 20:51:23 -0500

    add login component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Login from './components/Login';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+
+  state = {
+    isLoggedIn: false
+  };
+
+  setLogin = (value) => {
+    this.setState({isLoggedIn: value});
+  }
+
+  render () {
+    return (
+      <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          My Music App
+        </Toolbar>
+      </AppBar>
+
+      {
+        !this.state.isLoggedIn
+        ? <Login onLogin={this.setLogin} />
+        : 'Add Dashboard component'
+      }
     </div>
-  );
+    )
+  };
 }
-
-export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+import './style.css';
+
+export default class Login extends Component {
+    render() {
+       const { onLogin } = this.props;
+        return (
+            <div className="login-container">
+                <TextField label="Username" variant="outlined" />
+                <TextField label="Password" variant="outlined" />
+                <Button variant="contained" color="primary"
+                    onClick={() => onLogin(true)}>
+                    Login
+                </Button>
+            </div>
+        );
+    }
+}
