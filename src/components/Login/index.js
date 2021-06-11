97ed4b051660da709b6cdfb86dc9fbe724c9b3ed
Erik Zambrano <Erik.Zambrano@gartner.com>
2021-06-10 19:56:28 -0500

    apply material style

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,21 +1,38 @@
 import { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
+import { withStyles } from '@material-ui/core/styles';
 import './style.css';
 
-export default class Login extends Component {
+const styles = theme => ({
+    textField: {
+        marginBottom: theme.spacing(2)
+    }
+});
+
+class Login extends Component {
     render() {
-       const { onLogin } = this.props;
+       const { onLoginFunction, classes } = this.props;
+
         return (
-            <div className="login-container">
-                <TextField label="Username" variant="outlined" />
-                <TextField label="Password" variant="outlined" />
+            <div className="login-container {}">
+                <TextField
+                    className={classes.textField}
+                    label="Username"
+                    variant="outlined"
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Password"
+                    variant="outlined"
+                />
                 <Button variant="contained" color="primary"
-                    onClick={() => onLogin(true)}>
+                    onClick={() => onLoginFunction(true)}>
                     Login
                 </Button>
             </div>
         );
     }
 }
+
+export default withStyles(styles)(Login)
